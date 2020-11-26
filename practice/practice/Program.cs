6634da0e6026e6e6fea4c8d40c484541a5cfed09
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2020-11-26 12:34:12 -0700

    more practice

---
+++ practice/practice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            int MaxProfit(int[] prices)
+            {
+                if (prices.Length < 2) return 0;
+                int maxProfit = 0;
+                int lastPrice = prices[0];
+                for (int i = 1; i < prices.Length; i++)
+                {
+                    if (prices[i] > lastPrice)
+                        maxProfit += prices[i] - lastPrice;
+                    lastPrice = prices[i];
+
+                }
+                return maxProfit;
+            }
+            int removeDuplicates(int[] nums)
+            {
+                int index = 1;
+                for(int i = 0; i < nums.Length - 1; i++)
+                {
+                    if(nums[i] != nums[i + 1])
+                     {
+                        nums[index++] = nums[i + 1];
+                    }
+                }
+
+                return index;
+            }
+            int[]p = { 1, 1, 2 };
+
+             int a = removeDuplicates(p);
+
+            Console.WriteLine(a);
+
+            int[] q = { 7, 1, 5, 3, 6, 4 };
+
+            int q2a = MaxProfit(q);
+
+            Console.WriteLine(q2a);
+        }
+    }
+}
